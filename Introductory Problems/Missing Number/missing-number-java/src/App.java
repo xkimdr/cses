10f4c72169852daf5beacb8a59b8ae294e5552bd
Amit Srivastave <xkimdr@gmail.com>
2024-12-16 01:29:18 +0530

    performance(-): split

--- Introductory Problems/Missing Number/missing-number-java/src/App.java
+++ Introductory Problems/Missing Number/missing-number-java/src/App.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -11,12 +10,12 @@ public class App {
 
         final long sum1 = (n * (n + 1)) / 2;
 
-        StringTokenizer st = new StringTokenizer(buff.readLine());
-
         long sum2 = 0;
 
-        while (st.hasMoreTokens()) {
-            sum2 += Long.parseLong(st.nextToken());
+        final String[] strings = buff.readLine().split(" ");
+
+        for (String string : strings) {
+            sum2 += Long.parseLong(string);
         }
 
         System.out.println(sum1 - sum2);
