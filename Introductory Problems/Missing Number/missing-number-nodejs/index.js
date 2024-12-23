d6cc5b354ee612b8bf75eb6a119793c691bab9d4
Amit Srivastava <xkimdr@gmail.com>
2024-12-23 07:01:40 +0530

    no change in perfomance

--- Introductory Problems/Missing Number/missing-number-nodejs/index.js
+++ Introductory Problems/Missing Number/missing-number-nodejs/index.js
@@ -1,25 +1,23 @@
-const readline = require("readline");
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
-const processInput = (lines) => {
-  const n = parseInt(lines[0]);
-  const s1 = (n * (n + 1)) / 2;
-  const s2 = lines[1].split(" ").reduce((sum, x) => sum + parseInt(x), 0);
+const lines = [];
 
+const processInput = (lines) => {
+  const n = parseInt(lines[0], 10);
+  const s1 = (n * (n + 1)) / 2;
+  const s2 = lines[1].split(' ').reduce((acc, x) => acc + parseInt(x, 10), 0);
   console.log(s1 - s2);
 };
 
-const lines = [];
-let c = 0;
-rl.on("line", (line) => {
-  ++c;
-  lines.push(line);
+rl.on('line', (line) => {
+  lines.push(line.trim());
 
-  if (c == 2) {
+  if (lines.length === 2) {
     rl.close();
     processInput(lines);
   }
